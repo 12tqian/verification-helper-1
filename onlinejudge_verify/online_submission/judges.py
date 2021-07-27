14f14ee0de3256461a978be11b1cdaf29ac1b336
Timothy Qian <12tqian@gmail.com>
2021-07-26 20:21:02 -0400

    Update judges.py

--- onlinejudge_verify/online_submission/judges.py
+++ onlinejudge_verify/online_submission/judges.py
@@ -95,8 +95,8 @@ class VJudge:
         # Logging in
         
         options = Options()
-        # options.add_argument('--headless')
-        # options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')  # Last I checked this was necessary.
         driver = webdriver.Chrome(chrome_options=options)
         driver.get(self.JUDGE_URL)
         wait = WebDriverWait(driver, 10)
