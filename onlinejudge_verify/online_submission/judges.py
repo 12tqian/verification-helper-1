d78264df24202b16df6a414c2c8dfec15da05f87
12tqian <12tqian@gmail.com>
2022-07-22 21:09:12 -0400

    new webdriver

--- onlinejudge_verify/online_submission/judges.py
+++ onlinejudge_verify/online_submission/judges.py
@@ -143,16 +143,15 @@ class VJudge:
                 logger.error('Error signing in.')
                 
     def submit_solution(self, problem_link, solution): 
-        if self.driver is None:
-            options = Options()
-            options.add_argument('--headless')
-            options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')  # Last I checked this was necessary.
             
-            # display = Display(visible=False, size=(800, 800)) # for some reason this is necessary
-            # display.start()
-            # self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
-            
-            self.driver = webdriver.Chrome(chrome_options=options) # old version
+        # display = Display(visible=False, size=(800, 800)) # for some reason this is necessary
+        # display.start()
+        # self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+        
+        self.driver = webdriver.Chrome(chrome_options=options) # old version
         
         driver = self.driver 
         
